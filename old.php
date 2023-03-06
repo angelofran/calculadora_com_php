8005293625623c8709c30815a23facbffa9bba50
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-03-06 20:22:16 +0100

    Updated

--- old.php
+++ old.php
@@ -9,21 +9,30 @@
 </head>
 <body>
     <div class="container" style="display: flex; justify-content: center;margin-top: 200px;">
-        <form action="old.php" method="POST">
-            <h1>Olá, calcule a sua idade.</h1>
-            <div class="mb-3">
-                <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Write here year you were born" name="idade">
-            </div>
-            <select class="form-select" aria-label="Default select example" name="op">
-                <option selected disabled>Select an option</option>
-                <option value="1">Yes</option>
-                <option value="2">Not</option>
-            </select>
-            <input type="submit" value="Calcular" id="btn" class="btn btn-success" style="margin-top: 10px;">
-        </form>
+
+        <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $op = $_POST["op"] ?? 1;
+                $old = $_POST["idade"] ?? 2023;
+                $idade = date("Y") - $old;
+                if($op == 1){
+                    echo "Resultado: $idade."; 
+                }
+                else
+                {
+                    $idade =  $idade - 1;
+                    echo "Resulta: $idade.";
+                }
+            }
+            else {
+                echo "Method error ❌";
+            }   
+            // $_REQUEST junção de $_GET e $_POST   
+        ?>
     </div>
     
     <!--<p><a href="javascript:history.go(-1)">Voltar</a></p>-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 </html>
+
